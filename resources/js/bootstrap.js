a2205cad22deb77451f9290119346cf7c8737634
ephratta <ephraimoviij@gmail.com>
2022-06-11 02:02:04 +0100

    changed resources folder

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,31 +1,21 @@
-import _ from 'lodash';
-window._ = _;
+// Load plugins
+import helper from "./helper";
+import axios from "axios";
+import * as Popper from "@popperjs/core";
+import dom from "@left4code/tw-starter/dist/js/dom";
 
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-import axios from 'axios';
+// Set plugins globally
+window.helper = helper;
 window.axios = axios;
+window.Popper = Popper;
+window.$ = dom;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo';
-
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+// CSRF token
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
+} else {
+    console.error(
+        "CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token"
+    );
+}
